d645badfa18a38986e315e4e9ca6bafa5689dcc3
Steve-Kung <stevekung98@gmail.com>
2020-01-18 10:43:45 +0800

    200118 blog finish update02 bushu

--- src/main/java/com/steve/blog/BlogApplication.java
+++ src/main/java/com/steve/blog/BlogApplication.java
@@ -2,6 +2,24 @@ package com.steve.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//@ServletComponentScan
+//public class BlogApplication extends SpringBootServletInitializer {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(BlogApplication.class, args);
+//	}
+//
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(BlogApplication.class);
+//	}
+//}
 
 @SpringBootApplication
 public class BlogApplication {
@@ -11,3 +29,4 @@ public class BlogApplication {
 	}
 
 }
+
